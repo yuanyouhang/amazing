df3db6fdaeb09c63400b6b2e931463fd16981eac
yuanyouhang <yuan_you_hang@sina.com>
2023-06-12 12:01:59 +0800

    feat: 添加主题切换功能

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div class="h-screen flex flex-col">
+  <div class="h-screen flex flex-col dark:bg-slate-600 dark:text-white">
     <HelloWorld />
-    <router-view></router-view>
+    <main class="flex-1 mx-auto py-4 sm:px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 container">
+      <router-view></router-view>
+    </main>
   </div>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,43 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watchEffect } from 'vue'
 
-const count = ref(0)
+const setLightTheme = () => {
+  localStorage.theme = 'light'
+  document.documentElement.classList.remove('dark')
+}
+const setDarkTheme = () => {
+  localStorage.theme = 'dark'
+  document.documentElement.classList.add('dark')
+}
+if (!('theme' in localStorage)) {
+  setLightTheme()
+}
+
+const theme = ref(localStorage.theme)
+watchEffect(() => {
+  if (theme.value === 'light') {
+    setLightTheme()
+  }
+  else {
+    setDarkTheme()
+  }
+})
 </script>
 
 <template>
-  <h1 class="text-center italic text-2xl font-bold py-2">Hello, World!</h1>
+  <header class="py-2 sm:mx-4 md:mx-8 lg:mx-16 xl:mx-32 2xl:mx-64 flex relative">
+    <div class="italic text-2xl font-bold mx-auto">Hello, World!</div>
+    <div class="absolute right-0">
+      <el-switch
+        v-model="theme"
+        active-value="dark"
+        inactive-value="light"
+        inline-prompt
+        active-text="Dark"
+        inactive-text="Light"
+      />
+    </div>
+  </header>
 </template>
 
 <style scoped>
