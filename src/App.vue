38c4aabaee06223e06c91483171fd7eae3a876aa
yuanyouhang <yuan_you_hang@sina.com>
2023-06-15 21:00:07 +0800

    feat: 缓存已激活组件

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,14 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div class="h-screen flex flex-col dark:bg-slate-600 dark:text-white">
+  <div class="h-screen flex flex-col dark:bg-slate-700 dark:text-white">
     <HelloWorld />
     <main class="flex-1 mx-auto py-4 sm:px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 container">
-      <router-view></router-view>
+      <router-view v-slot="{ Component }">
+        <keep-alive>
+          <component :is="Component" />
+        </keep-alive>
+      </router-view>
     </main>
   </div>
 </template>
