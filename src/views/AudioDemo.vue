5e7f5e2c5b8873470a8b7db6e4af7c66ac9f2294
yuanyouhang <yuan_you_hang@sina.com>
2023-06-16 23:31:09 +0800

    feat: 新增改变音频可视化图像的颜色

--- src/views/AudioDemo.vue
+++ src/views/AudioDemo.vue
@@ -6,22 +6,26 @@
         class="text-white bg-sky-500 hover:bg-sky-400 cursor-pointer text-center block p-2 sticky top-0"
       >添加音频</label>
       <input type="file" accept="audio/*" id="selector" ref="audioSelector" style="display: none;" @change="selectFile">
-      <div
-        v-for="item, index in audios"
-        class="p-2 my-1 hover:bg-slate-200 cursor-pointer dark:hover:bg-slate-400"
-        @click="playAudio(item, index)"
-        :class="[{'bg-slate-300': index === currentPlayIndex}, {'dark:bg-slate-500': index === currentPlayIndex}]"
-      >
-        {{ item.name }}
-      </div>
+      <template v-if="audios.length">
+        <div
+          v-for="item, index in audios"
+          class="p-2 my-1 hover:bg-slate-200 cursor-pointer dark:hover:bg-slate-400"
+          @click="playAudio(item, index)"
+          :class="[{'bg-slate-300': index === currentPlayIndex}, {'dark:bg-slate-500': index === currentPlayIndex}]"
+        >
+          {{ item.name }}
+        </div>
+      </template>
+      <div v-else class="text-center text-stone-400 py-2">音频列表为空</div>
     </div>
-    <canvas width="650" height="500" id="canvas" class="bg-slate-200 rounded-r-xl"></canvas>
+    <canvas width="650" height="500" id="canvas" class="bg-slate-200 rounded-r-xl cursor-pointer" title="点击变色"></canvas>
   </div>
   <audio :src="audioSrc" ref="audioRef" controls class="mx-auto my-2"></audio>
 </template>
 
 <script setup>
 import { nextTick, onMounted, ref } from 'vue'
+import getColor from "@/utils/getRandomColor"
 
 let canvas, ctx, isInit
 onMounted(() => {
@@ -29,6 +33,10 @@ onMounted(() => {
   ctx = canvas.getContext('2d')
   ctx.translate(canvas.width / 2, canvas.height / 2)
   ctx.lineWidth = 1
+  ctx.lineCap = 'round'
+  canvas.addEventListener('click', () => {
+    ctx.strokeStyle = getColor()
+  })
   draw(new Array(360).fill(1))
   audioRef.value.addEventListener('ended', endPlay)
 })
@@ -53,9 +61,9 @@ const draw = (lines) => {
       len = lines[i]
     }
     ctx.beginPath()
-    // 80相当于内圆(空白区)的半径, len/1.5 为线的长度
+    // 80相当于内圆(空白区)的半径, len/* 为线的长度
     ctx.moveTo(80,0)
-    ctx.lineTo(80 + len / 1.5,0)
+    ctx.lineTo(80 + len / 1.3,0)
     ctx.stroke()
     ctx.rotate(Math.PI * 2 / lines.length)
   }
