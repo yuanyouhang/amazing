132e12192e142087f5feb565adbd72def4d43713
yuanyouhang <yuan_you_hang@sina.com>
2023-08-12 17:31:26 +0800

    feat: 新增"更多"页

--- src/views/More.vue
+++ src/views/More.vue
@@ -1,9 +1,31 @@
 <template>
-  <div>
-    更多。。。
+  <div class="h-full flex flex-col">
+    <div>
+      持续学习以创造更多：
+    </div>
+    <div class="flex-1 overflow-auto p-4 border mt-2 rounded shadow-inner">
+      <div class="h-full overflow-auto">
+        <div
+          v-for="item in sites"
+          class="mb-2"
+        >
+          {{ item.name }}（<a :href="item.url" target="_blank" class="hover:text-blue-400 hover:underline">{{item.url}}</a>）
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { inject, ref, onMounted } from 'vue'
 
+const api = inject('API')
+const sites =ref([])
+async function getSites() {
+  const res = await api.getSites()
+  sites.value = res.data
+}
+onMounted(() => {
+  getSites()
+})
 </script>
